95e523caa2b75603a6621eebc257efa46b164226
Kagiso <216013349@student.uj.ac.za>
2019-03-18 04:03:28 +0200

    add author and info to question

--- app/User.php
+++ app/User.php
@@ -41,4 +41,9 @@ class User extends Authenticatable
         return $this->hasmany(Question::class);
     }
 
+    public function getUrlAttribute(){
+     //   return route("questions.show",$this->id);
+     return '#';
+    }
+
 }
