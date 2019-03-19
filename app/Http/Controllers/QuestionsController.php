9c2a995078cc07a841884620ec90c45f249070c4
Kagiso <216013349@student.uj.ac.za>
2019-03-19 12:53:29 +0200

    auth using policy

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -8,6 +8,11 @@ use App\Http\Requests\AskQuestionRequest;
 
 class QuestionsController extends Controller
 {
+
+    public function __construct(){
+        $this->middleware('auth',['except' => 'index','show']);
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -62,6 +67,7 @@ class QuestionsController extends Controller
      */
     public function edit(Question $question)
     {
+        $this->authorize("update",$question);
         return view('questions.edit',compact('question'));
     }
 
@@ -74,6 +80,8 @@ class QuestionsController extends Controller
      */
     public function update(AskQuestionRequest $request, Question $question)
     {
+
+        $this->authorize("update",$question);
         $question->update($request->only('title','body'));
         return redirect('/questions')->with('success',"Your question has been updated");
     }
@@ -86,6 +94,7 @@ class QuestionsController extends Controller
      */
     public function destroy(Question $question)
     {
+        $this->authorize('delete',$question);
         $question->delete();
         return redirect('/questions')->with('success',"Your question has been deleted.");
     }
